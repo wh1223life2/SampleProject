748b0cb88d46ba8a345e2f89420e203e1a270e4f
WANG Hao <halewhp@gmail.com>
2022-11-08 19:05:24 +0800

    ?

--- src/hk/edu/polyu/comp/comp2021/simple/Application.java
+++ src/hk/edu/polyu/comp/comp2021/simple/Application.java
@@ -2,11 +2,14 @@ package hk.edu.polyu.comp.comp2021.simple;
 
 import hk.edu.polyu.comp.comp2021.simple.model.Simple;
 
+@SuppressWarnings("MissingJavadoc")
 public class Application {
 
+    @SuppressWarnings("MissingJavadoc")
     public static void main(String[] args){
         Simple simple = new Simple();
         // Initialize and utilize the system
+        123
     }
 
 }
