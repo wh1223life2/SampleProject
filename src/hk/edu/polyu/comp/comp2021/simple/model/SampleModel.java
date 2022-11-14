1301dcca5a0a8898d4742056c3cb808700169c7b
MioSena <jeremyqinfeng@outloo.com>
2022-11-14 19:46:22 +0800

    Lin Qinfeng

--- src/hk/edu/polyu/comp/comp2021/simple/model/SampleModel.java
+++ src/hk/edu/polyu/comp/comp2021/simple/model/SampleModel.java
@@ -1,7 +1,9 @@
 package hk.edu.polyu.comp.comp2021.simple.model;
 
 
+import hk.edu.polyu.comp.comp2021.simple.control.ExceptionController;
 import hk.edu.polyu.comp.comp2021.simple.control.InterpreterException;
+import jdk.vm.ci.meta.ExceptionHandler;
 
 import java.util.HashMap;
 
@@ -9,7 +11,30 @@ public class SampleModel {
 
     public static HashMap<String, String> boolvar = new HashMap<>();
     public static HashMap<String, Integer> intvar = new HashMap<String, Integer>();
-    public static void varDefine(Statement ?) throws InterpreterException {
+    public static void varDefine(Statement statement) throws InterpreterException {
+
+        String expression = statement.getExpression();
+        String[] expression_list = expression.split(" ");
+
+        String[] list = {};
+        int index = 0;
+        for(String part : expression_list){
+            list[index++] = part;
+        }
+
+        String part1_name = list[0];
+        String part2_type = list[1];
+        String part3 = list[2];
+
+        Integer part3_int = Integer.parseInt(part3);
+        if(part3_int % 1 == 0){
+            ExceptionController.handleErr(ExceptionController.SYNTAX);
+        }
+        if(part2_type != "int"){
+            ExceptionController.handleErr(ExceptionController.NOTVAR);
+        }
+        intvar.put(part1_name,part3_int);
+
 
 
        /* Statment = "float x 100"
