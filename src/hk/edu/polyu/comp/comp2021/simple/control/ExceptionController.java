5afefe174637f9dd73047ddeeaee82a581303b81
WANG Hao <halewhp@gmail.com>
2022-11-15 13:25:33 +0800

    11/15 读入更新

--- src/hk/edu/polyu/comp/comp2021/simple/control/ExceptionController.java
+++ src/hk/edu/polyu/comp/comp2021/simple/control/ExceptionController.java
@@ -1,5 +1,7 @@
 package hk.edu.polyu.comp.comp2021.simple.control;
 
+import static hk.edu.polyu.comp.comp2021.simple.control.SampleController.total;
+
 public class ExceptionController {
     public static final int SYNTAX = 0;
     public static final int NOVARTP = 1;
@@ -24,15 +26,40 @@ public class ExceptionController {
                 "Undefined label",
                 "Variable cannot be defined"
         };
-        throw new InterpreterException("<" + label + ">" + err[error]);
+        throw new InterpreterException("<" + label + "> " + err[error]);
     }
 
     /*
+    只检查操作类型 不检查run
     1.类型是否符合？
-    2.label是否重复（define）/label是否不存在？（run）
+    2.label是否重复（define）/label是否不存在？（run）[raw string内的]
     3.expression的表达是否属于该类型（合规？）-》只判断字符串数量 不判断分割后字符串内容是否合规
      */
-    public static boolean checkLegal(String type, String rawstring){
-        if(SampleController.RunExecuteType.contains())
+    public static void checkLegal(String type,String rawstring) throws InterpreterException {
+        String[] single = rawstring.split(" ");
+        if(!SampleController.checkUnique(single[0]))
+            handleErr(single[0],DUPLABEL);
+        else{
+            if(type.equals("vardef") || type.equals("binexpr") || type.equals("if")){
+                if(single.length != 4)
+                    handleErr(single[0],SYNTAX);
+            }
+            else if(type.equals("unexpr") || type.equals("assign") || type.equals("while")){
+                if(single.length != 3)
+                    handleErr(single[0],SYNTAX);
+            }
+            else if(type.equals("print") || type.equals("program")){
+                if(single.length != 2)
+                    handleErr(single[0],SYNTAX);
+            }
+            else if(type.equals("skip")){
+                if(single.length != 1)
+                    handleErr(single[0],SYNTAX);
+            }
+            else{
+                if(single.length < 2)
+                    handleErr(single[0],SYNTAX);
+            }
+        }
     }
 }
--- src/hk/edu/polyu/comp/comp2021/simple/control/SampleController.java
+++ src/hk/edu/polyu/comp/comp2021/simple/control/SampleController.java
@@ -9,33 +9,39 @@ public class SampleController {
     //public static HashMap<String,String> Labels = new HashMap<>();
     //public static HashMap<String,String> ExecuteType = new HashMap<>();
     public static HashSet<Statement> total = new HashSet<>();
-    public static List<String> DeExecuteType = Arrays.asList("vardef","binexpr","unexpr","assign","print","skip","block","if","while","program");
-    public static List<String> RunExecuteType = Arrays.asList("execute","list","store","load");
+    public static List<String> DeExecuteType = Arrays.asList("vardef", "binexpr", "unexpr", "assign", "print", "skip", "block", "if", "while", "program");
+    public static List<String> RunExecuteType = Arrays.asList("execute", "list", "store", "load");
+
     public static void Initial(String str) throws InterpreterException {
-        StringTokenizer currentSub = new StringTokenizer(str," ");
+        StringTokenizer currentSub = new StringTokenizer(str, " ");
         String temp1 = currentSub.nextToken();
-        if(!(DeExecuteType.contains(temp1) || RunExecuteType.contains(temp1)))
-            ExceptionController.handleErr(ExceptionController.NOEXPTP);
-        else if(!currentSub.hasMoreTokens())
-            ExceptionController.handleErr(ExceptionController.SYNTAX);
+        String temp2 = "";
+        if (!currentSub.hasMoreTokens())
+            ExceptionController.handleErr("unknown Statement", ExceptionController.SYNTAX);
+        else if (DeExecuteType.contains(temp1)) {
+            ExceptionController.checkLegal(temp1,str.substring(temp1.length()+1));
+            temp2 = currentSub.nextToken();
+            total.add(new Statement(temp1,temp2,str.substring(temp1.length() + temp2.length() + 2)));
+        }
+        /*else if (RunExecuteType.contains(temp2)) {
+
+        }*/
         else {
-            String temp2 = currentSub.nextToken();
-            if (DeExecuteType.contains(temp1)) {
-                if(temp1.equals("skip"))
-                    total.add(new Statement("skip",temp2,""));
-                else total.add(new Statement(temp1,temp2,str.substring(temp1.length() + temp2.length() + 2)));
-            }
-            else {
-                /*wait to be completed  temp2 belong to Run */
-            }
-            /*if(Statments.containsKey(temp2))
-                ExceptionController.handleErr(ExceptionController.DUPLABEL);*/
+            temp2 = currentSub.nextToken();
+            ExceptionController.handleErr(temp2, ExceptionController.NOEXPTP);
         }
     }
 
-    public static boolean checkUnique(String newlabel){//keep label unique
-        for(Statement p :total)
-            if(p.getLabel().equals(newlabel))
+    //keep label unique
+    public static boolean checkUnique(String newlabel) {
+        for (Statement p : total)
+            if (p.getLabel().equals(newlabel))
                 return false;
         return true;
+    }
+
+    public static void Testing(){
+        for (Statement p : total)
+            System.out.println(p.getOperationType() + " " + p.getLabel() + " " +p.getExpression());
+    }
 }
